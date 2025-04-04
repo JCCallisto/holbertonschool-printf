5f98ad3f4d23e853ee06e0866eb1eb7bbabd3d63
Jean Carlo Carrion Caraballo <jeancarrion8@gmail.com>
2025-04-04 11:33:18 -0400

    Printf

--- _printf_.c
+++ _printf_.c
@@ -1,57 +1,6 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * _printf - Custom printf function that prints the formatted output.
- * @format: The Format specifier strings.
- * @... : The values to format and print.
- * Return: Amount of words written.
- */
-
-int _printf(const char *format, ...)
-
-{
-	va_list args;
-	int total_written = 0;
-	const char *p;
-
-	va_start(args, format);
-	for (p = format; *p != '\0'; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-
-			switch (*p)
-			{
-				case 's':
-				{
-				char *s = va_arg(args, char*);
-				 total_written += write(1, s, strlen(s));
-				break;
-				}
-				case 'c':
-				{
-				int c = va_arg(args, int);
-				 total_written += write(1, &c, 1);
-				break;
-				}
-			{
-			default:
-		total_written += write(1, "%", 1);
-			total_written += write(1, p, 1);
-			break;
-			}
-		}
-		}
-	else
-	{
-		write(1, p, 1);
-	}
-}
-	va_end(args);
-	return (total_written++);
-=======
  * _printf - custom printf function
  * @format: string to print with specifiers
  *
@@ -124,5 +73,4 @@ int _printf(const char *format, ...)
 
 	va_end(args);
 	return (count);
->>>>>>> 76506c3a994ab86e92f6c38a4623614093a40c08
 }
